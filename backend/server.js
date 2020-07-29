c76e5d2a7d5a44ec0624e4ad0a5fb3b8ecf30af4
Rohit Agarwal <agarwal.rohit0910@gmail.com>
2020-07-29 14:36:51 +0530

    Fixed bugs

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path")
 
 const app = express();
 
@@ -19,7 +20,7 @@ app.use("/api/projects", require("./routes/api/projects"));
   app.use(express.static("../frontend/build"))
 
   app.get("*", (req, res) => {
-    res.sendFile("../frontend/build/index.html")
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
   })
 
 const PORT = process.env.PORT || 5000;
